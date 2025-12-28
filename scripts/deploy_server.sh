9aaad5aa50a4b1cd009e489c5bd32ea34be9d3df
volta2030 <volta2030@gmail.com>
2025-12-28 09:59:19 +0900

    Run Streamlit on port 8501

--- scripts/deploy_server.sh
+++ scripts/deploy_server.sh
@@ -1,1 +1,1 @@
-./.venv/bin/python -m streamlit run app.py || python3 -m streamlit run app.py || python -m streamlit run app.py
+python3 -m streamlit run app.py --server.port 8501 || python -m streamlit run app.py --server.port 8501
